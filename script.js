bfb7243bca542d1ff0f17be791666de79eaa8ab8
molly460 <vanessamolly21@gmail.com>
2022-03-28 20:42:39 +0100

    new updates

--- script.js
+++ script.js
@@ -1,42 +1,35 @@
-const SIGN = document.getElementById("but");
-const SIGNUP = document.getElementById("my-Form");
-let emailInput = document.getElementById("email");
-let passwordInput = document.getElementById("password");
+const Sign = document.querySelector(".sign-in-btn");
+const Signup = document.getElementById("Form-input");
+const emailInput = document.getElementById("email");
+const passwordInput = document.getElementById("password");
 
 const msg = document.getElementById('msg');
-SIGN.addEventListener('click', () => {
+Sign.addEventListener('click', () => {
 
-loginForm();
 
+  loginForm();
 
 });
+
 
 
 function loginForm() {
-    const Login = document.getElementById("my-Form").style.display ="block";
+    const Login = document.getElementById("Form-input").style.display ="block";
 }
 
-const x = document.getElementById("last-btn").addEventListener("click", () => {
 
-    formValidation();
-  
+const form = document.getElementById('last-btn').addEventListener('click', event => {
+  event.preventDefault();
 
-});
 
-function formValidation() {
-    
-  
-if (emailInput.value === "" || passwordInput ===""){
+  if (emailInput.value === '' || passwordInput.value === '') {
     msg.classList.add('error');
-   msg.innerHTML = 'please enter a valid email and password'; 
-   setTimeout(() => msg.remove(), 3000);
-
-} else {
-    console.log('success');
-}
-
-
-
-}
-
+    msg.innerHTML = 'please fill the input boxes with  a valid email and password';
+   
+    setTimeout(() => msg.remove(), 3000);
+      
+  }else{
+    console.log('success!');
+  }
+});
 
